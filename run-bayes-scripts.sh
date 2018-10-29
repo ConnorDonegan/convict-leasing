a2893bd3b3471b0233708b57fabeab11889b3344
ConnorDonegan <connordonegan@gmail.com>
2018-10-29 12:14:17 -0500

    update readme

--- run-bayes-scripts.sh
+++ run-bayes-scripts.sh
@@ -1,13 +1,24 @@
 
 
+# run scripts in this order to get figures and table with model estimated SIRs
 
-Rscript scripts/sentencing-rates-prep.R
+# interpolate county population between Decennial Censuses for 1900-1920
+Rscript scripts/data-prep/sentencing-rates-prep.R
 
-Rscript scripts/bayesian-model/bayes-data-prep.R
+# count total person-years and sentences by county, 
+ # calculate log of expected sentences,
+ # and add local empirical bayes estimates for mean sentencing count/rate/ratio
+Rscript scripts/data-prep/bayes-data-prep.R
 
+# fit multi-level bayesian poisson model to estimate 'shrunken' rates for each county
 Rscript scripts/bayesian-model/bayesian-model.R
 
-# compare model specifications
+# compare model specifications: distinct distribution of rates in plantation belt or not
 # Rscript scripts/bayesian-model/bayes-model-compare.R
 
-Rscripts scripts/bayesian-model/bayes-report-results.R
+# compare estimates and raw values by table of results and plot of all estimates
+Rscript scripts/figures/bayes-table-of-results.R
+Rscript scripts/figures/bayes-plot-estimates.R
+
+# make maps of plantation belt and standardized sentencing ratios
+Rscript scripts/figures/make-maps.R
